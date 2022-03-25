4817ead2c8c64eecd8d70a2ff3756281adc41e99
Vojtěch Jelínek <vojtech.jelinek@hey.com>
2022-03-25 13:04:21 +0100

    Force pylint to be 2.11.1 (#1)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author_email=about['__author_email__'],
     url=about['__url__'],
     install_requires=[
-        'pylint>=2.8.0',
+        'pylint==2.11.1',
     ],
     python_requires=">=3.6",
     packages=['pylint_quotes'],
